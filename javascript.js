ecd3c9126cdb3d54218c72321ca886190f78cc52
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-01-18 06:43:30 +0000

    Complete rock-paper-scissors simulation against computer

--- javascript.js
+++ javascript.js
@@ -17,4 +17,71 @@ function computerPlay() {
   }
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+
+  if (playerSelection.toLowerCase() === "rock" && computerSelection === "Paper") {
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1, playerSelection.length);
+    return "lost";
+  } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "Scissors") {
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1, playerSelection.length);
+    return "won";
+  } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "Rock") {
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1, playerSelection.length);
+    return "tied";
+  } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "Paper") {
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1, playerSelection.length);
+    return "won";
+  } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "Scissors") {
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1, playerSelection.length);
+    return "tied";
+  } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "Rock") {
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1, playerSelection.length);
+    return "lost";
+  } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Rock") {
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1, playerSelection.length);
+    return "won";
+  } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Paper") {
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1, playerSelection.length);
+    return "tied";
+  } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Scissors") {
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1, playerSelection.length);
+    return "lost";
+  }
+
+}
+
+function game() {
+
+  let playerScore = 0;
+  let computerScore = 0;
+
+  for (let i = 0; i < 5; i++) {
+
+    let playerSelection = prompt("Type Rock, Paper or Scissors: ");
+    let computerSelection = computerPlay();
+
+    let result = playRound(playerSelection, computerSelection);
+    if (result === "won") {
+      playerScore++;
+    } else if (result === "lost") {
+      computerScore++;
+    }
+
+    console.log("You " + result + " this round.");
+  }
+
+  if (playerScore > computerScore) {
+    console.log("You are the winner! Score " + playerScore + " - " + computerScore);
+  } else if (playerScore === computerScore) {
+    console.log("You tied the computer! Score " + playerScore + " - " + computerScore);
+  } else {
+    console.log("The computer wins... Score " + playerScore + " - " + computerScore);
+  }
+
+}
+
+const playerSelection = "rock";
+
+const computerSelection = computerPlay();
+
+game();
