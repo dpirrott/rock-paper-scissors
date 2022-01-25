cb3ad2ffbeeb4db9965f232e3b2115da47c7ada5
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-01-25 07:25:59 +0000

    Added the rocks theme song, will work on it more and need to do reset button still

--- javascript.js
+++ javascript.js
@@ -56,10 +56,6 @@ function gameOver() {
     choice.removeEventListener('click', play);
   });
 
-  endScore.innerText = playerScore + " - " + computerScore;
-  endScore.id = "endScore";
-  body.insertBefore(endScore, scoreContainer);
-
 }
 
 
@@ -74,96 +70,97 @@ function updateScore() {
 
     roundWinner.remove();
     if (playerScore > computerScore) {
-      gameMessage.setAttribute('style', 'font-size: 32px; color: green;');
-      gameMessage.innerText = "You won! :)";
-      endScore.style.color = 'green';
+      gameMessage.setAttribute('style', 'font-size: 32px; color: green; animation-delay: 1s;');
+      gameMessage.innerText = "You won! :)\n" + playerScore + " - " + computerScore;
     } else {
       gameMessage.setAttribute('style', 'font-size: 32px; color: red;');
-      gameMessage.innerText = "You lost :(";
-      endScore.style.color = 'red';
+      gameMessage.innerText = "You lost :(\n" + playerScore + " - " + computerScore;
     }
     gameOver();
   }
 }
 
 function play(e) {
-  let playerChoice = e.srcElement.id;
-  console.log(playerSelect.className);
-  if (playerSelect.className !== "") playerSelect.remove();
-  coordX = e.srcElement.getBoundingClientRect().x;
-  coordY = e.srcElement.getBoundingClientRect().y;
+  setTimeout(() => {
+    let playerChoice = e.srcElement.id;
+    console.log(playerSelect.className);
+    if (playerSelect.className !== "") playerSelect.remove();
+    coordX = e.srcElement.getBoundingClientRect().x;
+    coordY = e.srcElement.getBoundingClientRect().y;
+    
+    playerSelect.style.top = coordY;
+    playerSelect.style.left = coordX
+    //Determine which image will be duplicated to player side
+    if (playerChoice === "rock") {
+      playerSelect.src = rockImg.src;
+      playerSelect.className = "rockPlay";    
+    } else if (playerChoice === "scissors") {
+      playerSelect.src = "images/scissors (copy).png";
+      playerSelect.className = "scissorsPlay";
+    }else{
+      playerSelect.src = paperImg.src;
+      playerSelect.className = "paperPlay";
+    }
+    e.srcElement.parentNode.appendChild(playerSelect);
+    
+    // Now computers choice img will be duplicated to its play area
+    let computerChoice = computerPlay();
+    let computerChoiceLower = computerChoice.toLowerCase();
+    let choiceImg = document.getElementById(computerChoiceLower);
+    coordX = choiceImg.getBoundingClientRect().x;
+    coordX = choiceImg.getBoundingClientRect().y;
   
-  playerSelect.style.top = coordY;
-  playerSelect.style.left = coordX
-  //Determine which image will be duplicated to player side
-  if (playerChoice === "rock") {
-    playerSelect.src = rockImg.src;
-    playerSelect.className = "rockPlay";    
-  } else if (playerChoice === "scissors") {
-    playerSelect.src = "images/scissors (copy).png";
-    playerSelect.className = "scissorsPlay";
-  }else{
-    playerSelect.src = paperImg.src;
-    playerSelect.className = "paperPlay";
-  }
-  e.srcElement.parentNode.appendChild(playerSelect);
+    if (computerSelect.className !== "") computerSelect.remove();
+    computerSelect.style.left = coordX;
+    computerSelect.style.top = coordY;
+    computerSelect.className = "rockPlay";
+    if (computerChoiceLower === "rock") {
+      computerSelect.src = rockImg.src;
+      computerSelect.style.animationName = "rockComputer";
+    } else if (computerChoiceLower === "scissors") {
+      computerSelect.src = "images/scissors (copy).png";
+      computerSelect.style.animationName = "scissorsComputer";
+    } else {
+      computerSelect.src = paperImg.src;
+      computerSelect.style.animationName = "paperComputer";
+    }
+    choiceImg.parentNode.appendChild(computerSelect);
   
-  // Now computers choice img will be duplicated to its play area
-  let computerChoice = computerPlay();
-  let computerChoiceLower = computerChoice.toLowerCase();
-  let choiceImg = document.getElementById(computerChoiceLower);
-  coordX = choiceImg.getBoundingClientRect().x;
-  coordX = choiceImg.getBoundingClientRect().y;
-
-  if (computerSelect.className !== "") computerSelect.remove();
-  computerSelect.style.left = coordX;
-  computerSelect.style.top = coordY;
-  computerSelect.className = "rockPlay";
-  if (computerChoiceLower === "rock") {
-    computerSelect.src = rockImg.src;
-    computerSelect.style.animationName = "rockComputer";
-  } else if (computerChoiceLower === "scissors") {
-    computerSelect.src = "images/scissors (copy).png";
-    computerSelect.style.animationName = "scissorsComputer";
-  } else {
-    computerSelect.src = paperImg.src;
-    computerSelect.style.animationName = "paperComputer";
-  }
-  choiceImg.parentNode.appendChild(computerSelect);
-
-  let roundResult = playRound(playerChoice, computerChoice);
-  gameMessage.removeAttribute('id');
-  gameMessage.id = "gameMessage";
-  gameMessage.style.animationDelay = "0s";
-  gameMessage.style.color = 'black'
-  gameMessage.style.textDecoration = "underline";
-  gameMessage.innerText = 'Round winner';
-  roundWinner.id = "roundWinner";
-  roundWinner.style.textDecoration = "none";
-
-  playerChoice = playerChoice[0].toUpperCase() + playerChoice.slice(1, playerChoice.length);
-
-  // Initiate transition
-  messageContainer.replaceChild(blankNode, gameMessage);
-  messageContainer.replaceChild(gameMessage, blankNode);
-
-  // Remove transition for game message if one iteration has passed
-  if (roundWinner.innerText !== "") roundWinner.remove();
-
-  if (roundResult === 'won') {
-    playerScore++;
-    roundWinner.innerText = '<== Player';
-    roundWinner.style.color = 'green';
-  } else if (roundResult === 'lost') {
-    computerScore++;
-    roundWinner.innerText = 'Computer ==>';
-    roundWinner.style.color = 'red';
-  } else {
-    roundWinner.style.color = "black";
-    roundWinner.innerText = '<== Tied ==>';
-  }
-  messageContainer.appendChild(roundWinner);
-  updateScore();
+    let roundResult = playRound(playerChoice, computerChoice);
+    gameMessage.removeAttribute('id');
+    gameMessage.id = "gameMessage";
+    gameMessage.style.animationDelay = "250ms";
+    gameMessage.style.color = 'black'
+    gameMessage.style.textDecoration = "underline";
+    gameMessage.innerText = 'Round winner';
+    roundWinner.id = "roundWinner";
+  
+    playerChoice = playerChoice[0].toUpperCase() + playerChoice.slice(1, playerChoice.length);
+  
+    // Initiate transition
+    messageContainer.replaceChild(blankNode, gameMessage);
+    messageContainer.replaceChild(gameMessage, blankNode);
+  
+    // Remove transition for game message if one iteration has passed
+   // if (roundWinner.innerText !== "") roundWinner.remove();
+  
+    if (roundResult === 'won') {
+      playerScore++;
+      roundWinner.innerText = '<== Player';
+      roundWinner.style.color = 'green';
+    } else if (roundResult === 'lost') {
+      computerScore++;
+      roundWinner.innerText = 'Computer ==>';
+      roundWinner.style.color = 'red';
+    } else {
+      roundWinner.style.color = "black";
+      roundWinner.innerText = '<== Tied ==>';
+    }
+    messageContainer.appendChild(roundWinner);
+    
+    updateScore();
+  }, 200);
+  
 }
 
 function shake(e) {
@@ -243,6 +240,10 @@ function setupGameLayout() {
   flexPaper.appendChild(paperImg);
   flexScissors.appendChild(scissorsImg);
 
+  setTimeout(() => {
+    rockTheme.play();
+  }, 1250);
+
   // Scoreboard setup
   scoreContainer.id = "score-container";
   body.appendChild(scoreContainer);
@@ -261,7 +262,11 @@ function setupGameLayout() {
   
 
   //Acount for message heights
-  
+  messageContainer.id = "messageContainer"
+  body.appendChild(messageContainer);
+  gameMessage.id = 'gameMessage';
+  gameMessage.innerText = 'Fight!';
+  messageContainer.appendChild(gameMessage);
   
 }
 
@@ -286,26 +291,26 @@ function startGame() {
 
     setTimeout(() => {
       baitPhrase.style.animationDelay = "0s";
-      baitPhrase.style.animationDuration = "2s";
+      baitPhrase.style.animationDuration = "2s"; // set to 2s
       baitPhrase.style.opacity = "1";
       baitPhrase.style.animationName = "baitBounceBack";
       console.log("Animation duration: " + baitPhrase.style.animationDuration);
 
       epicStory.style.animationIterationCount = "1";
-      epicStory.style.animationDuration = "2s";
+      epicStory.style.animationDuration = "2s"; //set to 2s
       epicStory.style.animationName = "storyStartAgain";
 
       setTimeout(() => {
-        epicStory.style.animationDuration = "2s";
+        epicStory.style.animationDuration = "2s"; // set to 2s
         epicStory.style.animationName = "storyFinish";
         epicStory.innerText = "The worlds finest scientists have deemed\nthe following weapons our best hope.";
 
         setTimeout(() => {
-          epicStory.style.animationDuration = "3s";
+          epicStory.style.animationDuration = "3s"; // set to 3s
           epicStory.style.animationName = "storyHide";
 
           setTimeout(() => {
-            baitPhrase.style.animationDuration = "1s";            
+            baitPhrase.style.animationDuration = "1s"; // set to 1s        
             baitPhrase.style.fontWeight = "900";
             baitPhrase.style.lineHeight = "42px";
             baitPhrase.style.fontSize = "42px"
@@ -314,7 +319,7 @@ function startGame() {
             console.log("Animation duration: " + baitPhrase.animationDuration);
 
             setTimeout(() => {
-              baitPhrase.style.animationDuration = "3s";
+              baitPhrase.style.animationDuration = "3s"; // set to 3s
               baitPhrase.style.animationName = "fadeOut";
               baitPhrase.innerText = "Good luck!";
 
@@ -322,13 +327,13 @@ function startGame() {
               bait.style.animationName = "baitFadeOut";
               setTimeout(() => {
                 baitPhrase.remove();
-              }, 3000);
-            }, 5000)
+              }, 3000); // set to 3000
+            }, 5000) //set to 5000
           }, 3000)// set 3000
         }, 2000) // set 2000
       }, 2000)// set to 2000
-    }, 700)
-  }, 700);
+    }, 700) // set to 700
+  }, 700); // set to 700
   
   
   // Initialize new/reset game
@@ -340,9 +345,9 @@ function startGame() {
     
     choices = document.querySelectorAll('.flex-item');
 
-  }, 10500)
+  }, 10500) // set to 10500
   
-  setTimeout(eventListenerSetup, 15000);
+  setTimeout(eventListenerSetup, 15000);// set to 15000
   
 }
 
@@ -370,33 +375,36 @@ let epicStory = document.querySelector('.epicStory');
 let playerSelect = document.createElement('img');
 let computerSelect = document.createElement('img');
 let roundWinner = document.createElement('p');
+const rockTheme = new Audio('sounds/theRockTheme.mp3');
 
 setTimeout(() => {
   epicStory.style.animationDelay = 0;
   epicStory.innerText = "Will Smith showed us...";
   setTimeout(() => {
     epicStory.innerText = "It has finally happened...";
-    setTimeout(() => {
-      epicStory.innerText = "Powerful AI robots have taken control of the world";
+    setTimeout(() => {      
+      epicStory.innerText = "Powerful AI robots have taken control of the world";  
       setTimeout(() => {
         epicStory.style.animationName = "storyFinish";
         epicStory.style.animationDuration = "2s";
         epicStory.style.animationIterationCount = 1;
         epicStory.innerText = "We need a hero now";
-        baitPhrase.id = "baitPhrase";        
-        baitPhrase.innerText = "Will you save us?";
-        bait.appendChild(baitPhrase);
-        startBtn.id = "start";
-        startBtn.innerText = "Start";
-        bait.appendChild(startBtn);
-
-        startBtn.addEventListener('click', startGame, {
-          once: true
-        });
-      }, 100)
-    }, 100)
-  }, 100)
-}, 100)// These were all 4000
+        setTimeout(() => {
+          baitPhrase.id = "baitPhrase";        
+          baitPhrase.innerText = "Will you save us?";
+          bait.appendChild(baitPhrase);
+          startBtn.id = "start";
+          startBtn.innerText = "Start";
+          bait.appendChild(startBtn);
+          startBtn.addEventListener('click', startGame, {
+            once: true
+          });
+        }, 1000);  
+          
+      }, 4000)
+    }, 4000)
+  }, 4000)
+}, 4000)// These were all 4000
 
 
 
